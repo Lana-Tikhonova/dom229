252f13c08081fc02603676bfe59feda6985ef975
Svetlana <frontend.lana@gmail.com>
2026-05-01 10:03:23 +0500

    fix

--- assets/js/map.js
+++ assets/js/map.js
@@ -54,33 +54,40 @@ async function main() {
   );
 
 
-  // Координаты полигона 
-  const POLYGON_COORDINATES = [
-    [
-      [37.535, 55.385],
-      [37.545, 55.385],
-      [37.545, 55.390],
-      [37.535, 55.390],
-      [37.535, 55.385] // Замыкающая точка (такая же, как первая)
-    ]
-  ];
+  //   // Координаты полигона 
+  // const POLYGON_COORDINATES = [
+  //   [
+  //     [37.535, 55.385],
+  //     [37.545, 55.385],
+  //     [37.545, 55.390],
+  //     [37.535, 55.390],
+  //     [37.535, 55.385] // Замыкающая точка (такая же, как первая)
+  //   ]
+  // ];
 
-  // Создание полигона
-  const polygon = new YMapFeature({
-    geometry: {
-      type: 'Polygon',
-      coordinates: POLYGON_COORDINATES
-    },
-    style: {
-      fill: 'rgba(56, 128, 255, 0.5)', // Цвет заливки с прозрачностью
-      stroke: [{ color: '#3880ff', width: 4 }] // Цвет и толщина обводки
+  // // Создание полигона
+  // const polygon = new YMapFeature({
+  //   geometry: {
+  //     type: 'Polygon',
+  //     coordinates: POLYGON_COORDINATES
+  //   },
+  //   style: {
+  //     fill: 'rgba(56, 128, 255, 0.5)', // Цвет заливки с прозрачностью
+  //     stroke: [{color: '#3880ff', width: 4}] // Цвет и толщина обводки
+  //   }
+  // });
+
+  // // Добавление на карту
+  // map.addChild(polygon);
+
+
+
+  const listener = new ymaps3.YMapListener({
+    onClick: (object, event) => {
+      console.log(`[${event.coords[0].toFixed(6)}, ${event.coords[1].toFixed(6)}]`);
     }
   });
-
-  // Добавление на карту
-  map.addChild(polygon);
-
-
+  map.addChild(listener);
 
   // Создание маркера
   const markerElement = document.createElement('img');
